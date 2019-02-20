c6dddcb6ae77a96944c4288683df1900e9d31f75
Serhii Stepanenko <sstepane@macmini5478.ddns.softservecom.com>
2019-02-20 16:40:34 +0200

    forms and methods added

---
+++ src/app.js
@@ -0,0 +1,35 @@
+
+import { Customer } from './customer';
+
+export class App {
+  constructor() {
+    this.heading = 'Customer Manager';
+    this.customers = [];
+
+    this.customerName = '';
+    this.customerEmail = '';
+    this.customerPhone = '';
+  }
+
+  addCustomer() {
+    if (this.customerName && this.customerEmail && this.customerPhone) {
+      this.customers.push(new Customer(
+        this.customerName,
+        this.customerEmail,
+        this.customerPhone
+      ));
+    }
+
+    this.customerName = '';
+    this.customerEmail = '';
+    this.customerPhone = '';
+  }
+
+  removeCustomer(customer) {
+    let index = this.customers.indexOf(customer);
+
+    if (index !== -1 ) {
+      this.customers.splice(index, 1);
+    }
+  }
+}
---
+++ src/customer.js
@@ -0,0 +1,8 @@
+
+export class Customer {
+  constructor(name, email, phone) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+  }
+}
