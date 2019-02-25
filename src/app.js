672ba796374e448e508d7c525281ab856e5a8f30
Serhii Stepanenko <sstepane@macmini5478.ddns.softservecom.com>
2019-02-25 16:05:18 +0200

    added local storage and my solutions

--- src/app.js
+++ src/app.js
@@ -4,12 +4,24 @@ import { Customer } from './customer';
 export class App {
   constructor() {
     this.heading = 'Customer Manager';
-    this.customers = [];
+    this.customers = JSON.parse(localStorage.getItem('customers'));
 
     this.customerName = '';
     this.customerEmail = '';
     this.customerPhone = '';
   }
+
+  // getCustomersFromStorage() {
+  //   let customers = JSON.parse(localStorage.getItem('customers'));
+
+  //   if (!customers) {
+  //     customers = [];
+  //   }
+
+  //   return customers;
+  // } // Brads longer version
+
+  // created() { this.customers = JSON.parse(localStorage.getItem('customers')); } // my solution with created() hook
 
   addCustomer() {
     if (this.customerName && this.customerEmail && this.customerPhone) {
@@ -20,16 +32,49 @@ export class App {
       ));
     }
 
+    // store in local storage - LS only stores strings (not objects)
+    localStorage.setItem('customers', JSON.stringify(this.customers));
+    // this.storeCustomer(this.customerName, this.customerEmail, this.customerPhone);
+
     this.customerName = '';
     this.customerEmail = '';
     this.customerPhone = '';
   }
 
+  // storeCustomer(name, email, phone) {
+  //   let customers;
+
+  //   if (localStorage.getItem('customers') === null) {
+  //     customers = [];
+  //   } else {
+  //     customers = JSON.parse(localStorage.getItem('customers'));
+  //   }
+
+  //   customers.push({
+  //     name: name,
+  //     email: email,
+  //     phone: phone
+  //   });
+
+  //   localStorage.setItem('customers', JSON.stringify(customers));
+  // } // Brads version
+
   removeCustomer(customer) {
     let index = this.customers.indexOf(customer);
 
-    if (index !== -1 ) {
+    if (index !== -1) {
       this.customers.splice(index, 1);
+      localStorage.setItem('customers', JSON.stringify(this.customers)); // my solution
+      // this.removeFromStorage(index); // Brads version
     }
   }
+
+  // removeFromStorage(index) {
+  //   let customers = JSON.parse(localStorage.getItem('customers'));
+
+  //   customers.splice(index, 1);
+
+  //   localStorage.setItem('customers', JSON.stringify(this.customers));
+  // } // Brads version
+
 }
